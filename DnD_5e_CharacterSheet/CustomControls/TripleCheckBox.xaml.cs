baf5e01111412a106463ea3e34bcca45ee2eab17
Fred Hallock <faha223@uky.edu>
2017-02-12 20:24:23 -0500

    Added a user control for the triple checkbox thing that's in the death saves box.
    Switched the Initiative, Speed, Hit Dice, and Death Saves boxes over to using the groupbox style.
    
    Simplified the code for the OrnateBox1 style for the GroupBox by using copies of the path for the corners.

---
+++ DnD_5e_CharacterSheet/CustomControls/TripleCheckBox.xaml.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace DnD_5e_CharacterSheet.CustomControls
+{
+    /// <summary>
+    /// Interaction logic for TripleCheckBox.xaml
+    /// </summary>
+    public partial class TripleCheckBox : UserControl
+    {
+        public bool IsChecked1
+        {
+            get { return (bool)GetValue(IsChecked1Property); }
+            set { SetValue(IsChecked1Property, value); }
+        }
+
+        // Using a DependencyProperty as the backing store for IsChecked1.  This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty IsChecked1Property =
+            DependencyProperty.Register("IsChecked1", typeof(bool), typeof(TripleCheckBox), new FrameworkPropertyMetadata(false, FrameworkPropertyMetadataOptions.BindsTwoWayByDefault));
+        
+        public bool IsChecked2
+        {
+            get { return (bool)GetValue(IsChecked2Property); }
+            set { SetValue(IsChecked2Property, value); }
+        }
+
+        // Using a DependencyProperty as the backing store for IsChecked2.  This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty IsChecked2Property =
+            DependencyProperty.Register("IsChecked2", typeof(bool), typeof(TripleCheckBox), new FrameworkPropertyMetadata(false, FrameworkPropertyMetadataOptions.BindsTwoWayByDefault));
+        
+        public bool IsChecked3
+        {
+            get { return (bool)GetValue(IsChecked3Property); }
+            set { SetValue(IsChecked3Property, value); }
+        }
+
+        // Using a DependencyProperty as the backing store for IsChecked3.  This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty IsChecked3Property =
+            DependencyProperty.Register("IsChecked3", typeof(bool), typeof(TripleCheckBox), new FrameworkPropertyMetadata(false, FrameworkPropertyMetadataOptions.BindsTwoWayByDefault));
+
+        public TripleCheckBox()
+        {
+            InitializeComponent();
+        }
+    }
+}
