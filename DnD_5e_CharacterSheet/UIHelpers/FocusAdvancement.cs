db92419ac15eba1eebac642adf2829dee4bc73f0
fred.hallock <fred.hallock@PC-FRED>
2017-02-17 09:57:47 -0500

    Reordered some things to make the tab order more intuitive
    Added a UI Helper to allow enter key and up/down keys to advance to next/previous control like tab/shift+tab does on the spellcasting page

---
+++ DnD_5e_CharacterSheet/UIHelpers/FocusAdvancement.cs
@@ -0,0 +1,43 @@
+﻿using System.Windows;
+using System.Windows.Input;
+
+namespace DnD_5e_CharacterSheet.UIHelpers
+{
+    public static class FocusAdvancement
+    {
+        public static bool GetAdvancesByEnterKey(DependencyObject obj)
+        {
+            return (bool)obj.GetValue(AdvancesByEnterKeyProperty);
+        }
+
+        public static void SetAdvancesByEnterKey(DependencyObject obj, bool value)
+        {
+            obj.SetValue(AdvancesByEnterKeyProperty, value);
+        }
+
+        public static readonly DependencyProperty AdvancesByEnterKeyProperty =
+            DependencyProperty.RegisterAttached("AdvancesByEnterKey", typeof(bool), typeof(FocusAdvancement),
+            new UIPropertyMetadata(OnAdvancesByEnterKeyPropertyChanged));
+
+        static void OnAdvancesByEnterKeyPropertyChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
+        {
+            var element = d as UIElement;
+            if (element == null) return;
+
+            if ((bool)e.NewValue) element.PreviewKeyDown += PreviewKeyDown;
+            else element.PreviewKeyDown -= PreviewKeyDown;
+        }
+
+        static void PreviewKeyDown(object sender, KeyEventArgs e)
+        {
+            if (!e.Key.Equals(Key.Enter) && !e.Key.Equals(Key.Down) && !e.Key.Equals(Key.Up)) return;
+
+            var dir = FocusNavigationDirection.Next;
+            if (e.Key.Equals(Key.Up))
+                dir = FocusNavigationDirection.Previous;
+
+            var element = sender as UIElement;
+            if (element != null) element.MoveFocus(new TraversalRequest(dir));
+        }
+    }
+}
